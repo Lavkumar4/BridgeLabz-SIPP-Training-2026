7ec12e5439a60708d40d07c974b0e843b090fc1c
Lavkumar4 <lavsharma662@gmail.com>
2026-06-16 13:12:50 +0530

    core-java-practice

---
+++ core-java-practice/gcr-codebase/Recursion/PowerRecursion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PowerRecursion {
+    static long power(int x, int n) {
+        // Base case
+        if (n == 0) {
+            return 1;
+        }
+        // Recursive case
+        return x * power(x, n - 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(power(x, n));
+        sc.close();
+    }
+}
